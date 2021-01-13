0835b89330a22ad3ffeda61ed98890ca73e542a2
alumi-mic <kaship24@gmail.com>
2021-01-13 21:11:40 +0900

    NOTE: ちばもく会のルームアクションを停止

--- discord_app/src/discord_bot.py
+++ discord_app/src/discord_bot.py
@@ -26,25 +26,32 @@ async def on_message(message):
         app_logger.debug("Talk from {} : / Talk User : {} / Talk User Id : {} / Message : {}".format(message.channel.name, message.author.display_name, message.author.id, message.content.split(" ")[1]))
         
         if message.mentions[0].display_name == "mocrat":
-            if message.channel.name == "朝活もくもく会":
-                mocrat_actions = AsakatsuRoomAction(message)
-                post_item_arr = mocrat_actions.return_post_items()
+            #
+            # ちばもく会のルームアクションを停止
+            #
 
-            elif  message.channel.name == "もくもく会":
-                mocrat_actions = Moku2RoomAction(message)
-                post_item_arr = mocrat_actions.return_moku2_post_items()
+            # if message.channel.name == "朝活もくもく会":
+            #     mocrat_actions = AsakatsuRoomAction(message)
+            #     post_item_arr = mocrat_actions.return_post_items()
 
-            elif message.channel.name == "振り返り部屋":
-                mocrat_actions = FurikaeriRoomAction(message)
-                post_item_arr = mocrat_actions.return_post_items()
+            # elif  message.channel.name == "もくもく会":
+            #     mocrat_actions = Moku2RoomAction(message)
+            #     post_item_arr = mocrat_actions.return_moku2_post_items()
+
+            # elif message.channel.name == "振り返り部屋":
+            #     mocrat_actions = FurikaeriRoomAction(message)
+            #     post_item_arr = mocrat_actions.return_post_items()
             
-            elif message.channel.name == "作業ログの部屋":
-                mocrat_actions = LearningLogRoom(message)
-                post_item_arr = mocrat_actions.return_post_items()
+            # elif message.channel.name == "作業ログの部屋":
+            #     mocrat_actions = LearningLogRoom(message)
+            #     post_item_arr = mocrat_actions.return_post_items()
 
-            else:
-                mocrat_actions = GenericRoomAction(message)
-                post_item_arr = mocrat_actions.return_generic_post_items()
+            # else:
+            #     mocrat_actions = GenericRoomAction(message)
+            #     post_item_arr = mocrat_actions.return_generic_post_items()
+
+            mocrat_actions = GenericRoomAction(message)
+            post_item_arr = mocrat_actions.return_generic_post_items()
 
             for post in post_item_arr:
                 await message.channel.send(post)
